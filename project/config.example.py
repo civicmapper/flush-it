87ada42ea713aa3e29c06d58b59821d9a4feb828
Christian Gass <christiangass@outlook.com>
2017-11-21 22:11:15 -0500

    readme and sample config

--- project/config.example.py
+++ project/config.example.py
@@ -1,7 +1,7 @@
 # project/_config.py
 
 import os
-#from datetime import timedelta
+# from datetime import timedelta
 
 # Grabs the folder where the script runs.
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -10,27 +10,29 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 DEBUG = True
 
 # Secret key for session management.
-SECRET_KEY = ''
+SECRET_KEY = ""
 
 # Session lifetime (matches lifetime of Esri tokens)
 # PERMANENT_SESSION_LIFETIME = timedelta(seconds=3600)
-
-# ESRI IDs for accessing to premium AGOL services (elevation and hydrology)
-ESRI_APP_ID =''
-ESRI_APP_SECRET=''
 
 # ROKTECH CREDS for accessing 3RWW & CivicMapper ArcGIS Server services
 ROK_USER = ''
 ROK_PW = ''
 ROK_AUTH_URL = 'https://arcgis4.roktech.net/arcgis/tokens/generateToken'
-ROK_REFERER_URL = 'flush-it.civicmapper.com/'
 ROK_CLIENT_TYPE = 'requestip'
 #ROK_CLIENT_TYPE = 'referer'
+ROK_REFERER_URL = 'flush-it.civicmapper.com'
 
-# CivicMapper AGS CREDS for accessing CivicMapper ArcGIS Server demo services for 3RWW
-CMG_USER = ''
-CMG_PW = ''
-CMG_AUTH_URL = 'https://geo.civicmapper.com/arcgis/tokens/generateToken'
-CMG_REFERER_URL = 'flush-it.civicmapper.com/'
-CMG_CLIENT_TYPE = 'requestip'
-#CMG_CLIENT_TYPE = 'referer'
+# AGS CREDS for accessing CivicMapper demo services
+CMAGS_USER = ''
+CMAGS_PW = ''
+CMAGS_AUTH_URL = 'https://geodata.civicmapper.com/arcgis/tokens/generateToken'
+CMAGS_CLIENT_TYPE = 'requestip'
+#CMAGS_CLIENT_TYPE = 'referer'
+CMAGS_REFERER_URL = 'flush-it.civicmapper.com'
+
+# AGOL CREDS for accessing 3RWW ArcGIS Online
+ESRI_APP_CLIENT_ID = ''
+ESRI_APP_CLIENT_SECRET = ''
+ESRI_APP_TOKEN_EXPIRATION = '-1'
+ESRI_AUTH_URL = 'https://www.arcgis.com/sharing/oauth2/token'
